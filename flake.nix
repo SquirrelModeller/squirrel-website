7100303aa27a52262a48445dd2210a9d0e7a4863
Squirrel Modeller <squirrelmodeller@protonmail.com>
2026-03-18 21:39:16 +0100

    refactor: nix packaging and static prerender

--- flake.nix
+++ flake.nix
@@ -1,30 +1,77 @@
 {
-  description = "Website development flake";
+  description = "Squirrel website";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
+  outputs = {nixpkgs, ...}: let
     systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
     forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
-
-    mkPkgs = system: import nixpkgs {inherit system;};
   in {
-    devShells = forAllSystems (
-      system: let
-        pkgs = mkPkgs system;
-      in {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            pnpm
-            nodejs
-          ];
+    devShells = forAllSystems (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs
+          pnpm
+        ];
+      };
+    });
+
+    packages = forAllSystems (system: let
+      pkgs = import nixpkgs {inherit system;};
+      lib = pkgs.lib;
+
+      filteredSrc = lib.fileset.toSource {
+        root = ./.;
+        fileset = lib.fileset.unions [
+          ./src
+          ./static
+          ./package.json
+          ./pnpm-lock.yaml
+          ./svelte.config.js
+          ./vite.config.js
+          ./tsconfig.json
+          ./jsconfig.json
+        ];
+      };
+    in {
+      default = pkgs.stdenv.mkDerivation {
+        pname = "squirrel-website";
+        version = "1.0.0";
+        src = filteredSrc;
+
+        nativeBuildInputs = [
+          pkgs.nodejs
+          pkgs.pnpm
+          pkgs.pnpmConfigHook
+        ];
+
+        pnpmDeps = pkgs.fetchPnpmDeps {
+          pname = "squirrel-website";
+          version = "1.0.0";
+          src = filteredSrc;
+          fetcherVersion = 3;
+          hash = "sha256-REPLACE_ME";
         };
-      }
-    );
+
+        buildPhase = ''
+          runHook preBuild
+          export HOME="$TMPDIR"
+          pnpm install --offline --frozen-lockfile
+          pnpm build
+          runHook postBuild
+        '';
+
+        installPhase = ''
+          runHook preInstall
+          mkdir -p "$out"
+          cp -r build/* "$out"/
+          runHook postInstall
+        '';
+      };
+    });
   };
 }
